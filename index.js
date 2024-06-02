8ac3ae3b21e4484c18ab23e439b4bd2f787411af
Pulkit Jain <pulkitjainhk@gmail.com>
2024-06-02 12:39:06 +0530

    Changing Port number

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.use("/movies", movieRouter)
 
-const PORT = 3001;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
